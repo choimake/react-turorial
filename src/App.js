680adfad652a024a7a9e7670859213b3f2a60e2f
choimake <choimake330@gmail.com>
2020-12-26 08:44:09 +0900

    add eslint, husky, lint-staged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+/**
+ * App
+ * @return {JSX.Element}
+ */
 function App() {
   return (
     <div className="App">
