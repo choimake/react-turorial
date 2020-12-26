d2a642b0fb3c5095cb0c94dd2d313d3a7095d0b1
choimake <choimake330@gmail.com>
2020-12-26 19:59:26 +0900

    props-typeを導入

--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Square = () => {
+const Square = (props) => {
   return (
     <>
-      <button className="square"></button>
+      <button className="square">{props.value}</button>
     </>
   );
+};
+
+/**
+ * props-typeの機能を使って、propsの型を定義している
+ * note: https://zenn.dev/h_yoshikawa0724/articles/2020-09-23-react-proptypes
+ */
+Square.propTypes = {
+  value: PropTypes.number,
 };
 
 export default Square;
