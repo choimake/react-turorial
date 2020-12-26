9e8567cbcc28e21353192dc1b6b86e7b4a7b36d5
choimake <choimake330@gmail.com>
2020-12-26 21:25:21 +0900

    Squareコンポーネントの表示処理を関数化

--- src/Board.js
+++ src/Board.js
@@ -14,23 +14,26 @@ const Board = () => {
     setSquare(s);
     turnChange();
   };
+  const renderSquare = (index) => {
+    return <Square value={square[index]} onClick={() => handleClick(index)} />;
+  };
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={square[0]} onClick={() => handleClick(0)} />
-        <Square value={square[1]} onClick={() => handleClick(1)} />
-        <Square value={square[2]} onClick={() => handleClick(2)} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={square[3]} onClick={() => handleClick(3)} />
-        <Square value={square[4]} onClick={() => handleClick(4)} />
-        <Square value={square[5]} onClick={() => handleClick(5)} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={square[6]} onClick={() => handleClick(6)} />
-        <Square value={square[7]} onClick={() => handleClick(7)} />
-        <Square value={square[8]} onClick={() => handleClick(8)} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </>
   );
