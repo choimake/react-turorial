82532be55c8e989f329d714f452bbe0c4c4fa47e
choimake <choimake330@gmail.com>
2020-12-26 22:32:21 +0900

    gameの進行状態をGameで管理するよう変更

--- src/Board.js
+++ src/Board.js
@@ -1,59 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import Square from "./Square";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const judgeTheWinner = () => {
-    // 一方向のマスの値が全て同じ値で揃えたユーザーの勝ち
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (const line of lines) {
-      const [first, second, third] = line;
-      if (
-        squares[first] &&
-        squares[first] === squares[second] &&
-        squares[first] === squares[third]
-      ) {
-        return squares[first];
-      }
-    }
-    return null;
-  };
-  const turnChange = () => {
-    setXIsNext(!xIsNext);
-  };
-  const handleClick = (index) => {
-    const s = squares.slice();
-    // すでに値が入力されている場合には何もしない
-    if (s[index]) {
-      return;
-    }
-    // 勝者が確定している場合には、何もしない
-    if (judgeTheWinner()) {
-      return;
-    }
-    s[index] = xIsNext ? "X" : "O";
-    setSquares(s);
-    turnChange();
-  };
-  const status = judgeTheWinner()
-    ? "Winner: " + judgeTheWinner()
-    : "Next Player: " + (xIsNext ? "X" : "O");
+const Board = (props) => {
   const renderSquare = (index) => {
-    return <Square value={squares[index]} onClick={() => handleClick(index)} />;
+    return (
+      <Square
+        value={props.squares[index]}
+        onClick={() => props.onClick(index)}
+      />
+    );
   };
   return (
     <>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -71,6 +30,11 @@ const Board = () => {
       </div>
     </>
   );
+};
+
+Board.propTypes = {
+  squares: PropTypes.array,
+  onClick: PropTypes.func,
 };
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 
 const Game = () => {
+  // gameの履歴
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // const [history] = useState([{ squares: Array(9).fill(null) }]);
+  const handleClick = (index) => {
+    const s = squares.slice();
+    // すでに値が入力されている場合には何もしない
+    if (s[index]) {
+      return;
+    }
+    // 勝者が確定している場合には、何もしない
+    if (judgeTheWinner()) {
+      return;
+    }
+    s[index] = xIsNext ? "X" : "O";
+    setSquares(s);
+    turnChange();
+  };
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const judgeTheWinner = () => {
+    // 一方向のマスの値が全て同じ値で揃えたユーザーの勝ち
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const line of lines) {
+      const [first, second, third] = line;
+      if (
+        squares[first] &&
+        squares[first] === squares[second] &&
+        squares[first] === squares[third]
+      ) {
+        return squares[first];
+      }
+    }
+    return null;
+  };
+  const turnChange = () => {
+    setXIsNext(!xIsNext);
+  };
+
+  const status = judgeTheWinner()
+    ? "Winner: " + judgeTheWinner()
+    : "Next Player: " + (xIsNext ? "X" : "O");
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <div className="status">{status}</div>
+        <Board squares={squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info"></div>
     </div>
